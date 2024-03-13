e46e3e4d4b33acd7f31983f44c79b0d507406b31
Gyanshu-kumar <gyanshusingh152@gmail.com>
2024-03-13 17:00:17 +0530

    first commit

---
+++ server/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+require("dotenv/config");
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// user authentication routes
+const userRoute = require("./routes/auth");
+app.use("/api/users/", userRoute);
+
+// Artist links
+const artistsRoute = require("./routes/artists");
+app.use("/api/artists/", artistsRoute);
+
+// Album links
+const albumRoute = require("./routes/albums");
+app.use("/api/albums/", albumRoute);
+
+// Songs links
+const songRoute = require("./routes/songs");
+app.use("/api/songs/", songRoute);
+
+
+
+mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true });
+mongoose.connection
+  .once("open", () => console.log("Connected"))
+  .on("error", (error) => {
+    console.log(`Error : ${error}`);
+  });
+
+app.listen(4000, () => console.log("lisitening to port 4000"));
